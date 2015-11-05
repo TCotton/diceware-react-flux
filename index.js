a507a526eb9d89318263a7a345b6bae973b44da3
Andy Walpole <me@andywalpole.me>
2015-11-05 17:21:29 +0000

    configured security features for nodejs

--- index.js
+++ index.js
@@ -10,11 +10,69 @@ let compress = require('compression');
 let favicon = require('serve-favicon');
 let cookieParser = require('cookie-parser');
 let methodOverride = require('method-override');
+let helmet = require('helmet');
+let csp = require('helmet-csp');
 
 const app = express();
 const sixMonths = 14515200;
 
 if (app.get('env') === 'production') {
+
+  app.use(helmet.frameguard('deny'));
+  app.use(helmet.xssFilter());
+  app.use(helmet.nosniff());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.hsts({maxAge: 15552000}));
+
+  app.use(csp({
+    // Specify directives as normal
+    defaultSrc: [
+      '\'self\''
+    ],
+    scriptSrc: [
+      '\'self\''
+    ],
+    styleSrc: [
+      '\'self\'',
+      'https://fonts.googleapis.com'
+    ],
+    imgSrc: [
+      '\'self\'',
+      'data:'
+    ],
+    fontSrc: [
+      '\'self\'',
+      'https://fonts.gstatic.com'
+    ],
+    frameSrc: [
+      '\'self\''
+    ],
+    sandbox: [
+      'allow-forms',
+      'allow-scripts',
+      'allow-same-origin'
+    ],
+    // Set to an empty array to allow nothing through
+    objectSrc: [
+      '\'self\''
+    ],
+    connectSrc: [
+      '\'self\''
+    ],
+
+    // Set to true if you only want browsers to report errors, not block them
+    reportOnly: false,
+
+    // Set to true if you want to blindly set all headers: Content-Security-Policy,
+    // X-WebKit-CSP, and X-Content-Security-Policy.
+    setAllHeaders: false,
+
+    // Set to true if you want to disable CSP on Android.
+    disableAndroid: true,
+
+    // Set to true if you want to force buggy CSP in Safari 5.1 and below.
+    safari5: false
+  }));
 
   app.use(compress());
   app.use(bodyParser.urlencoded({
@@ -27,6 +85,12 @@ if (app.get('env') === 'production') {
   app.set('port', process.env.PORT || 3000);
 
   app.use(favicon(path.join(__dirname, './build/favicon.ico')));
+
+  app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+  });
 
   app.all('*', function(req, res, next) {
     res.header('Cache-Control', 'no-cache');
@@ -55,6 +119,7 @@ if (app.get('env') === 'production') {
     });
   });
 
-  app.listen(app.get('port'), () => {});
+  app.listen(app.get('port'), () => {
+  });
 
 }
